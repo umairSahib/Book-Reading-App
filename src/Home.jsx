5ae99a808eba1d8535d4b8600edaac0cc52d8d1b
Your Name <your_email@example.com>
2024-07-22 11:38:56 +0500

    navbar

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import SideBar from "./component/SideBar";
 import Header from "./component/Header";
+import Navbar from "./component/Navbar";
 
 const Home = () => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      <Navbar />
     </>
   );
 };
