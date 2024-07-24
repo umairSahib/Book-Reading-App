c17148c56330a643ba8e670f87f0e19d2047cbc2
Your Name <your_email@example.com>
2024-07-24 21:09:56 +0500

    not find

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import Header from "./component/Header";
 import SideBar from "./component/SideBar";
 import All from "./component/All";
-import BookDetail from "./component/BookDetail";
 
 const App = () => {
   const [books, setBooks] = useState([]);
@@ -16,7 +15,6 @@ const App = () => {
           <All books={books} setBooks={setBooks} />
         </div>
       </div>
-      {/* <BookDetail /> */}
     </>
   );
 };
